24554960f465d8eeed1aed1ca1135cab2a2f9beb
pausarobe <32068920+pausarobe@users.noreply.github.com>
2023-03-29 13:43:42 +0200

    import AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent],
   imports: [
     BrowserModule
   ],
